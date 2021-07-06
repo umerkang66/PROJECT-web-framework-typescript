4e8f65c9423e6f743f7b4b7390724999a63cbcc9
umerkang66 <umerkang66@gmail.com>
2021-07-06 18:24:35 +0500

    added constructor in the UserList.ts file

--- src/views/CollectionView.ts
+++ src/views/CollectionView.ts
@@ -2,17 +2,13 @@ import { Collection } from '../models/Collection';
 
 export abstract class CollectionView<T, K> {
   constructor(public parent: Element, public collection: Collection<T, K>) {}
-
   abstract renderItem(model: T, itemparent: Element): void;
 
   render(): void {
     this.parent.innerHTML = '';
-
     const templateElement = document.createElement('template');
-
     this.collection.model.forEach((model: T): void => {
       const itemParent = document.createElement('div');
-
       this.renderItem(model, itemParent);
       templateElement.content.append(itemParent);
     });
--- src/views/UserList.ts
+++ src/views/UserList.ts
@@ -1,10 +1,13 @@
-import { ROOT_URL } from '../config';
 import { Collection } from '../models/Collection';
 import { User, UserProps } from '../models/User';
 import { CollectionView } from './CollectionView';
 import { UserShow } from './UserShow';
 
 export class UserList extends CollectionView<User, UserProps> {
+  constructor(rootId: Element, collection: Collection<User, UserProps>) {
+    super(rootId, collection);
+  }
+
   renderItem(model: User, parent: Element) {
     const userShow = new UserShow(parent, model);
     userShow.render();
